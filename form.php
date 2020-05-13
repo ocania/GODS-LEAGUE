c31909715f52fb9d5ce90493eddde10ca6e5b4f2
Adrián Ocaña <adrian.ocana@inslapineda.cat>
2020-05-14 01:36:17 +0200

    1.2
    Uso de php para el envio del resultado del formulario por correo.

---
+++ form.php
@@ -0,0 +1,9 @@
+<?php
+    $destino = "adrian.ocana@inslapineda.cat";
+    $nombre = $_POST["nombre"];
+    $mail = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+    $contenido = "Nombre:" . $nombre . "\nEmail: " . $mail . "\nMensaje:" . $mensaje;
+    mail($destino,"CONTACTO FORMULARIO", $contenido);
+    header("location:contacto.html")
+?>
